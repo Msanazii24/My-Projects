6a03698b89156f7644b115f2fb0d698a146edc2b
SanZoi <212407710+Msanazii24@users.noreply.github.com>
2025-12-07 18:48:52 +0100

    Add files via upload

---
+++ free games.bat
@@ -0,0 +1,28 @@
+@echo off
+echo Opening SteamDB free game pages...
+echo Make sure you are logged in on SteamDB before running this.
+
+REM === EXAMPLE SAFE FREE PERMANENT PACKAGES ===
+start "" "https://steamdb.info/app/570/"
+start "" "https://steamdb.info/app/440/"
+start "" "https://steamdb.info/app/304930/"
+start "" "https://steamdb.info/app/230410/"
+start "" "https://steamdb.info/app/1085660/"
+start "" "https://steamdb.info/app/275850/"
+start "" "https://steamdb.info/app/1286830/"
+start "" "https://steamdb.info/app/322330/"
+start "" "https://steamdb.info/app/582010/"
+start "" "https://steamdb.info/app/1172470/"
+start "" "https://steamdb.info/app/1599340/"
+start "" "https://steamdb.info/app/433850/"
+start "" "https://steamdb.info/app/1237970/"
+start "" "https://steamdb.info/app/1966720/"
+start "" "https://steamdb.info/app/1623730/"
+start "" "https://steamdb.info/app/1625450/"
+start "" "https://steamdb.info/app/304390/"
+start "" "https://steamdb.info/app/291550/"
+start "" "https://steamdb.info/app/760160/"
+start "" "https://steamdb.info/app/431960/"
+
+echo All pages opened.
+pause
