7896f362b75aacffd9135be6aa08ec119c4f761f
SanZoi <212407710+Msanazii24@users.noreply.github.com>
2025-12-07 18:46:49 +0100

    Add files via upload

---
+++ oppcc.py
@@ -0,0 +1,15 @@
+def calcul():
+    a=int(input("a="))
+    b=int(input("b="))
+    op=input("operateur=")
+    if op=="+":
+        print(a+b)
+    elif op=="-":
+        print(a-b)
+    elif op=="*":
+        print(a*b)
+    elif op=="/":
+        print(a/b)
+calcul()
+    
+
